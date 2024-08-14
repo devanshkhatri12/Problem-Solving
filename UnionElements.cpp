3e3428f185d5cfcb5facfafcf4f8638645ee6775
devanshkhatri12 <dkkhatri941@gmail.com>
2024-08-14 17:45:49 +0530

    Optimal approach

--- UnionElements.cpp
+++ UnionElements.cpp
@@ -30,11 +30,69 @@ void unionElem(vector<int>& arr1, vector<int>& arr2)
     }
 }
 
+// Optimal approach - TC - O(n + m) , SC - O(n + m)
+vector<int> unionElem1(vector<int>& arr1, vector<int>& arr2)
+{
+    int n = arr1.size();
+    int m = arr2.size();
+
+    vector<int> unionArr;
+
+    int i=0, j=0;
+    while(i<n && j<m)
+    {
+        if(arr1[i] <= arr2[j])
+        {
+            if(unionArr.back() != arr1[i] || unionArr.size() == 0)
+            {
+                unionArr.push_back(arr1[i]);
+            }
+            i++;
+        }
+        else
+        {
+            if(unionArr.back() != arr2[j] || unionArr.size() == 0)
+            {
+                unionArr.push_back(arr2[j]);
+            }
+            j++;
+        }
+    }
+
+    while(i<n)
+    {
+        if(unionArr.back() != arr1[i] || unionArr.size() == 0)
+        {
+            unionArr.push_back(arr1[i]);
+        }
+        i++;
+    }
+
+    while(j<m)
+    {
+        if(unionArr.back() != arr2[j] || unionArr.size() == 0)
+        {
+            unionArr.push_back(arr2[j]);
+        }
+        j++;
+    }
+
+    return unionArr;
+}
+
 int main()
 {
     vector<int> arr1 = {1, 1, 2, 3, 4, 5};
     vector<int> arr2 = {2, 3, 4, 4, 5, 6};
 
     unionElem(arr1, arr2);
+
+    vector<int> Union = unionElem1(arr1, arr2);
+
+    for(auto val : Union)
+    {
+        cout<<val<<" ";
+    }
+    cout<<endl;
 
 }
