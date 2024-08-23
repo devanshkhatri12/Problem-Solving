8ed8c75c8d49d14291fe4b92f44b8a2faad7155b
devanshkhatri12 <dkkhatri941@gmail.com>
2024-08-23 17:46:43 +0530

    Better approach

--- 2Sum.cpp
+++ 2Sum.cpp
@@ -22,6 +22,27 @@ vector<int> twoSum(vector<int>& arr, int target)
     return {-1, -1};
 }
 
+// Better approach - approx: TC - O(n^2) , SC - O(1);
+vector<int> twoSum(vector<int>& arr, int target)
+{
+    int n = arr.size();
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(i == j) continue;
+
+            if(arr[i] + arr[j] == target)
+            {
+                return  {i , j};
+            }
+        }
+    }
+
+    return {-1, -1};
+}
+
 int main()
 {
     int target;
