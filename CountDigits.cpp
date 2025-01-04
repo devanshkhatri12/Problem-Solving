d20fd6d5d9d730223660551fe5f9dde4df207c93
devansh khatri <dkkhatri941@gmail.com>
2025-01-04 18:22:16 +0530

    count total no. of digit

---
+++ CountDigits.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int evenlyDivides(int N){
+        //code here
+        int count = 0;
+        int temp = N;
+        
+        while(N > 0)
+        {
+            int lastdigit = N % 10;
+            
+            if(lastdigit != 0 && temp % lastdigit == 0)
+            {
+                count += 1;
+            }
+            
+            N = N / 10;
+        }
+        
+        return count;
+        
+    }
+
+int main()
+{
+    int n; 
+    cout<<"Enter no"<<endl;
+    cin>>n;
+
+    int ans = evenlyDivides(n);
+
+    cout<<"total no. of digits which divides exaclty: "<<ans<<endl;
+
+
+}
