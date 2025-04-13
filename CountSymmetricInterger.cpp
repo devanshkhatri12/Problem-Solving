8c996c694fc7649ece734ba9254fabe376d17af0
devanshkhatri12 <dkkhatri941@gmail.com>
2025-04-13 18:47:22 +0530

    counting number of symmetric integr btw low and high

---
+++ CountSymmetricInterger.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countPairs(int low , int high)
+{
+    int count = 0;
+    for(int i=low; i<=high; i++)
+    {
+        string s = to_string(i);
+
+        int len = s.length();
+
+        if(len%2 != 0){
+            continue;
+        }
+
+
+        int first_half = 0;
+        int second_half =0;
+        
+
+        for(int i=0; i<len/2; i++)
+        {
+            first_half += s[i] - '0';
+        }
+
+
+        for(int i=len/2; i<len; i++)
+        {
+            second_half += s[i] - '0';
+        }
+
+        if(first_half == second_half)
+        {
+            count += 1;
+        }
+    }
+
+    return count;
+
+}
+
+
+int main()
+{
+    int low , high;
+
+    cout<<"Enter low and high"<<endl;
+    cin>>low>>high;
+
+    int ans = countPairs(low ,high);
+
+    cout<<"Number of symmertric pairs are: "<<ans<<endl;
+}
