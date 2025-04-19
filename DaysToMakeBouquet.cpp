5b8183d13db3cd4cceffbb2961778200cc436e19
devanshkhatri12 <dkkhatri941@gmail.com>
2025-04-19 16:00:54 +0530

    Optimal appraoch: using Binary search

--- DaysToMakeBouquet.cpp
+++ DaysToMakeBouquet.cpp
@@ -26,6 +26,39 @@ bool possible(vector<int>& bloomDay, int day, int bouquek, int flower)
 }
 
 // Brute approach: using Linear approach: TC -O(N) ,O(1)
+// int MinimumBloomDay(vector<int>& bloomDay, int m, int k)
+// {
+//     int n = bloomDay.size();
+
+//     int mini = INT_MAX;
+//     int maxi = INT_MIN;
+
+//     for(int i=0; i<n; i++)
+//     {
+//         mini = min(mini , bloomDay[i]);
+//         maxi = max(maxi, bloomDay[i]);
+//     }
+
+//     int value = m * 1LL * k * 1LL;
+
+//     // edge case if total number of flower is less
+//     if(value > n)
+//     {
+//         return -1;
+//     }
+
+//     int ans = -1;
+
+//     for(int i=mini; i<=maxi; i++)
+//     {
+//         if(possible(bloomDay, i, m, k)){
+//             return i;
+//         }
+//     }
+//     return ans;
+// }
+
+// Optimal approach: TC - O(logn) , SC - O(1)
 int MinimumBloomDay(vector<int>& bloomDay, int m, int k)
 {
     int n = bloomDay.size();
@@ -48,20 +81,26 @@ int MinimumBloomDay(vector<int>& bloomDay, int m, int k)
     }
 
     int ans = -1;
-
-    for(int i=mini; i<=maxi; i++)
+    
+    while(mini <= maxi)
     {
-        if(possible(bloomDay, i, m, k)){
-            return i;
+        int mid = mini + (maxi -mini)/2;
+
+        if(possible(bloomDay, mid, m, k) >= true){
+            ans = mid;
+            maxi = mid-1;
+        }else{
+            mini = mid+1;
         }
     }
+
     return ans;
 }
 
 
 int main()
 {
-    vector<int> bloomDay = {7,7,7,7,13,11,12,7};
+    vector<int> bloomDay = {7,7,7,7,12,7,7};
 
     int m;
     cout<<"no. of bouquek you want to make"<<endl;
