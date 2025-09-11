97c65e439f864afbe7f3c07c2937291ea89541c4
devanshkhatri12 <dkkhatri941@gmail.com>
2025-09-11 18:56:00 +0530

    Check whether 2 strings are anagram are not - Optimal approach

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -21,6 +21,39 @@ bool validAnagram(string s, string t)
     return true;
 }
 
+// Optimal approach : TC - O(n) , SC - O(1)
+bool validAnagram(string s, string t)
+{
+
+    int n = s.length();
+    int m = t.length();
+
+    if(n != m)
+    return false;
+
+    unordered_map<char, int> mp(n);
+
+    for(int i=0; i<n; i++)
+    {
+        mp[s[i]]++;
+    }
+
+    for(int i=0; i<m; i++)
+    {
+        mp[t[i]]--;
+    }
+
+    for(auto it : mp)
+    {
+        if(it.second > 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     string s, t;
