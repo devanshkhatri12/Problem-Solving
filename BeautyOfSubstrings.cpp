4b5b2abd853f3ef36d8462c858d7ba54892e78b3
devanshkhatri12 <dkkhatri941@gmail.com>
2025-12-03 21:30:32 +0530

    Calculate Sum of Beauty of All substrings

---
+++ BeautyOfSubstrings.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Brute approach : TC - O(n^3) , SC - O(1)
+int countFreq(string str)
+{
+    int n = str.length();
+    map<char , int> mp;
+
+    for(int i=0; i<n; i++)
+    {
+        mp[str[i]]++;
+    }
+
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
+
+    for(auto it : mp)
+    {
+        if(it.second < mini)
+        {
+            mini = it.second;
+        }
+
+        if(it.second > maxi)
+        {
+            maxi = it.second;
+        }
+    }
+
+    return maxi - mini;
+}
+
+int getBeauty(string s)
+{
+    int n = s.length();
+    int count = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i; j<n; j++)
+        {
+            count += countFreq(s.substr(i , j-i+1));
+        }
+    }
+
+    return count;
+}
+
+
+
+
+
+int main()
+{
+    string s;
+    cout<<"enter string "<<endl;
+    getline(cin , s);
+
+    int ans = getBeauty(s);
+    cout<<"Sum of Beauty of All Substrings : "<<ans<<endl;
+}
