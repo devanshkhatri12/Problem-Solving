1bf5ec9e7d47527e9328892ab08ba66497592594
devanshkhatri12 <dkkhatri941@gmail.com>
2025-12-04 10:09:10 +0530

    remove some comments

--- BeautyOfSubstrings.cpp
+++ BeautyOfSubstrings.cpp
@@ -2,50 +2,50 @@
 using namespace std;
 
 // Brute approach : TC - O(n^3) , SC - O(1)
-// int countFreq(string str)
-// {
-//     int n = str.length();
-//     map<char , int> mp;
+int countFreq(string str)
+{
+    int n = str.length();
+    map<char , int> mp;
 
-//     for(int i=0; i<n; i++)
-//     {
-//         mp[str[i]]++;
-//     }
+    for(int i=0; i<n; i++)
+    {
+        mp[str[i]]++;
+    }
 
-//     int mini = INT_MAX;
-//     int maxi = INT_MIN;
+    int mini = INT_MAX;
+    int maxi = INT_MIN;
 
-//     for(auto it : mp)
-//     {
-//         if(it.second < mini)
-//         {
-//             mini = it.second;
-//         }
+    for(auto it : mp)
+    {
+        if(it.second < mini)
+        {
+            mini = it.second;
+        }
 
-//         if(it.second > maxi)
-//         {
-//             maxi = it.second;
-//         }
-//     }
+        if(it.second > maxi)
+        {
+            maxi = it.second;
+        }
+    }
 
-//     return maxi - mini;
-// }
+    return maxi - mini;
+}
 
-// int getBeauty(string s)
-// {
-//     int n = s.length();
-//     int count = 0;
+int getBeauty(string s)
+{
+    int n = s.length();
+    int count = 0;
 
-//     for(int i=0; i<n; i++)
-//     {
-//         for(int j=i; j<n; j++)
-//         {
-//             count += countFreq(s.substr(i , j-i+1));
-//         }
-//     }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i; j<n; j++)
+        {
+            count += countFreq(s.substr(i , j-i+1));
+        }
+    }
 
-//     return count;
-// }
+    return count;
+}
 
 // Optimal approach : TC - O(n^2) , SC - O(1)
 int getBeauty(string s)
