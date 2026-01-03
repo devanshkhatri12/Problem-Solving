0fe69f6350be0e941af42402a2eb143a18994143
devanshkhatri12 <dkkhatri941@gmail.com>
2026-01-03 19:39:35 +0530

    Rotate the list in right direction in k times

---
+++ RotateList.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+
+    public:
+    int data;
+    node* next;
+
+    node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+void printList(node* &head)
+{
+    node* temp = head;
+
+    while(temp != nullptr)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+node* takeInput()
+{
+    int x;
+    cin>>x;
+
+    node* head = nullptr;
+    node* tail = nullptr;
+
+    while(x != -1)
+    {
+        node* newNode = new node(x);
+
+        if(head == nullptr)
+        {
+            head = tail = newNode;
+
+        }else{
+
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin>>x;
+    }
+
+    return head;
+}
+
+
+// Brute approach : TC - O(3N) , sc - O(N)
+node* rotateRight(node* head, int k)
+    {
+
+        node* temp = head;
+        vector<int> list;
+        
+
+        while(temp != nullptr)
+        {
+            list.push_back(temp->data);
+            temp = temp->next;
+        }
+
+        int n = list.size();
+        vector<int> v(n, 0);
+
+        for(int i=0; i<n; i++)
+        {
+            v[(i+k)%n] = list[i];
+        }
+
+        temp = head;
+        int i=0;
+
+        while(temp != nullptr)
+        {
+            temp->data = v[i];
+            i++;
+
+            temp = temp->next;
+        }
+
+        return head;
+    }
+
+int main()
+{
+    int k;
+    cout<<"input k"<<endl;
+    cin>>k;
+
+    cout<<"enter list elements"<<endl;
+    node* head = takeInput();
+
+    cout<<"List before rotate k times"<<endl;
+    printList(head);
+
+    rotateRight(head, k);
+
+    cout<<"List before rotate k times"<<endl;
+    printList(head);
+
+    return 0;
+
+}
