bd4cfb69f4ab1551d14363b6e67fd14293558bbb
devanshkhatri12 <dkkhatri941@gmail.com>
2026-01-09 11:46:19 +0530

    finding power of x^n - Better approach

--- Power.cpp
+++ Power.cpp
@@ -5,8 +5,31 @@ using namespace std;
 void power(double x, int n)
 {
     double ans = pow(x,n);
-
     cout<<"Power of x^n is: "<<ans<<setprecision(4)<<endl;
+}
+
+// Better approach : TC - O(N) , SC - O(1)
+void power(double x, int n)
+{
+    double ans = 1;
+
+    // base case
+    if(n == 0 || x == 1.0) ans = 1;
+
+    long long temp = n;
+
+    if(n < 0)
+    {
+        x = 1/x;
+        temp = -1*1LL*n;
+    }
+
+    for(long long i=0; i<temp; i++)
+    {
+        ans *= x;
+    }
+
+    cout<<"Power of x^n is: "<<ans<<endl;
 }
 
 int main()
