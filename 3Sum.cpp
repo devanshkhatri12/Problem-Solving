94ee252bf97e1bb0b87eb7a0934aa3afd0a22409
devanshkhatri12 <dkkhatri941@gmail.com>
2026-03-27 00:45:02 +0530

    3 Sum - Brute approach

---
+++ 3Sum.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> Sum(vector<int>& num){
+    int n = num.size();
+    set<vector<int>> st;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            for(int k=j+1; k<n; k++)
+            {
+                if(num[i] + num[j] + num[k] == 0){
+                    vector<int> temp = {num[i] , num[j], num[k]};
+                    sort(temp.begin() , temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+
+    return vector<vector<int>>(st.begin() , st.end());
+
+}
+
+int main()
+{
+    int n=6;
+
+    vector<int> num(n);
+
+    cout<<"enter elements"<<endl;
+    for(int i=0; i<num.size(); i++)
+    {
+        cin>>num[i];
+    }
+
+
+    vector<vector<int>> ans = Sum(num);
+
+    for(int i=0; i<ans.size(); i++)
+    {
+        for(int j=0; j<ans[i].size(); j++)
+        {
+            cout<<ans[i][j]<<" ";
+        }cout<<endl;
+    }
+
+    return 0;
+
+
+}
