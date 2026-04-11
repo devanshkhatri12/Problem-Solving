9745b49687325d6c0ca08476e1703cf29350a44a
devanshkhatri12 <dkkhatri941@gmail.com>
2026-04-11 16:40:39 +0530

    3Sum - Optimal approach: Two-pointer

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -49,6 +49,50 @@ vector<vector<int>> Sum(vector<int>& num)
     return vector<vector<int>>(st.begin() , st.end());
 }
 
+// optimal approach: TC - nlogn + O(n*m), SC - O(no. of triplets)
+vector<vector<int>> Sum(vector<int>& num)
+{
+    int n = num.size();
+    vector<vector<int>> ans;
+
+    sort(num.begin() , num.end());
+
+    for(int i=0; i<n; i++){
+        if(i>0 && num[i] == num[i-1]) continue;
+
+        int j=i+1;
+        int k=n-1;
+
+        while(j<k)
+        {
+            int sum = num[i] + num[j] + num[k];
+            if(sum < 0){
+                j++;
+            }else if(sum > 0)
+            {
+                k--;
+            }
+            else{
+                vector<int> temp = {num[i] + num[j] + num[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+
+                while(j<k && num[j] == num[j-1]){
+                    j++;
+                }
+
+                while(j<k && num[k] == num[k+1]){
+                    k--;
+                }
+            }
+        }
+
+    }
+
+    return ans;
+}
+
 int main()
 {
     int n=6;
