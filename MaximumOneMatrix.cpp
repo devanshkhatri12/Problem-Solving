8814faa16cde53bea3f1e655d53226bae9808aae
devanshkhatri12 <dkkhatri941@gmail.com>
2026-05-03 19:01:55 +0530

    Find maximum number of one's in row and return row index - Optimal approach

--- MaximumOneMatrix.cpp
+++ MaximumOneMatrix.cpp
@@ -31,6 +31,51 @@ int Mat(vector<vector<int>>& matrix, int n, int m)
     return index;
 }
 
+int lowerBound(vector<int>& arr, int n, int target)
+{   
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+
+    while(low <= high)
+    {
+        int mid = low + (high-low)/2;
+
+        if(arr[mid] >= target)
+        {
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+
+    return ans;
+}
+
+// Optimal approach : TC - O(n * logm), SC - O(1)
+int Mat(vector<vector<int>>& matrix, int n, int m)
+{   
+    int countMax = 0;
+    int index = -1;
+
+    for(int i=0;i<n;i++)
+    {   
+        // 1s = total - first index of 1
+        int countOne = m - lowerBound(matrix[i], m, 1);
+
+        if(countOne > countMax)
+        {
+            countMax = countOne;
+            index = i;
+        }
+    }
+
+    return index;
+
+}
+
 int main() {
     
     vector<vector<int>> matrix = {{1, 0, 0}, {1, 0, 1}, {0, 0, 0}};
