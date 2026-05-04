7cff676dd45f89a21b688f39032349ab9c83cc09
devanshkhatri12 <dkkhatri941@gmail.com>
2026-05-04 17:01:33 +0530

    Search Element in 2D matrix - Optimal approach

--- SearchIn2DMatrix.cpp
+++ SearchIn2DMatrix.cpp
@@ -53,6 +53,31 @@ bool search(vector<vector<int>>& matrix, int n, int m, int target)
 
     return false;
 }
+
+// Optimal approach : TC - O(n), SC - O(1)
+bool search(vector<vector<int>>& matrix, int target) {
+
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    // starting/ intial point to start traversal
+    int i=0, j=m-1;
+
+    while(i < n && j >= 0)
+    {
+        if(matrix[i][j] == target){
+            return true;
+        }
+        else if(matrix[i][j] > target)
+        {
+            j--;
+        }else{
+            i++;
+        }
+    }
+
+    return false;
+}
 int main()
 {
     // int n, m;
