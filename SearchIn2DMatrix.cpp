dd74c0b4bfefe3fff4cf1ab2b0e6774827076d42
devanshkhatri12 <dkkhatri941@gmail.com>
2026-05-05 11:24:00 +0530

    Search Element in 2D matrix - Optimal approach

--- SearchIn2DMatrix.cpp
+++ SearchIn2DMatrix.cpp
@@ -55,10 +55,9 @@ bool search(vector<vector<int>>& matrix, int n, int m, int target)
 }
 
 // Optimal approach : TC - O(n), SC - O(1)
-bool search(vector<vector<int>>& matrix, int target) {
+bool search(vector<vector<int>>& matrix, int n, int m, int target) 
+{
 
-    int n = matrix.size();
-    int m = matrix[0].size();
     
     // starting/ intial point to start traversal
     int i=0, j=m-1;
@@ -78,11 +77,33 @@ bool search(vector<vector<int>>& matrix, int target) {
 
     return false;
 }
+
+// Optimal approach : TC - O(log(n*m)) , SC - O(1)
+bool search(vector<vector<int>>& matrix, int n, int m, int target) 
+{
+
+    int low = 0;
+    int high = (n*m)-1;
+
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+
+        if(matrix[mid/m][mid%m] == target){
+            return true;
+        }
+        else if(matrix[mid/m][mid%m] < target){
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+
+    return false;
+}
 int main()
 {
-    // int n, m;
-    // cout<<"enter the size of row and column"<<endl;
-    // cin>>n>>m;
+
 
     int target;
     cout<<"enter the element to search"<<endl;
